2e112dd031543035d279d35a91e11952fa827305
chrisreeves70 <chrisreeves70@icloud.com>
2024-08-16 12:09:56 -0700

    Update delete_user.php

---
+++ delete_user.php
@@ -0,0 +1,22 @@
+<?php
+// Database connection (same as in index.php)
+$dsn = 'mysql:host=us-cdbr-east-01.cleardb.net;dbname=heroku_82f3c661d2b7b36';
+$username = 'bb9db01117ded9';
+$password = 'ae365e5b';
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM users WHERE id = ?";
+$statement = $pdo->prepare($sql);
+$statement->execute([$id]);
+
+header("Location: index.php");
+exit;
+?>
