b1067813eddd4196d5a88e83268466a78bf5bf30
chrisreeves70 <chrisreeves70@icloud.com>
2024-08-17 21:42:40 -0700

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -13,6 +13,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Test query
+$result = $conn->query("SELECT 1");
+if ($result === false) {
+    die("Query failed: " . $conn->error);
+} else {
+    echo "Simple query executed successfully<br>";
+    flush();
+}
+
 // Collect POST data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
@@ -48,4 +57,5 @@ $conn->close();
     Email: <input type="email" name="email" required>
     <input type="submit" value="Add User">
 </form>
+
 
