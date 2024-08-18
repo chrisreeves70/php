f89926d1caf2bdc43904fc762acef60fa2ad7624
chrisreeves70 <chrisreeves70@icloud.com>
2024-08-18 10:36:11 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "us-cluster-east-01.k8s.cleardb.net";
+$username = "bb9db01117ded9";
+$password = "ae365e5b";
+$dbname = "heroku_82f3c661d2b7b36";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+$conn->close();
+?>
